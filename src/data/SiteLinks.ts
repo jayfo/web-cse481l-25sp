ed0a00ad72ff9e7d0abe74d19c7532c1977417f3
James Fogarty <jayfo@users.noreply.github.com>
2025-03-30 11:51:08 -0700

    Cleanup

--- src/data/SiteLinks.ts
+++ src/data/SiteLinks.ts
@@ -18,11 +18,6 @@ export const SiteLinks = {
     anchor: "Contribution Statements",
   },
 
-  // assignmentsReadingsTop: {
-  //   href: "/assignments/readings/#readingsanddiscussionposts",
-  //   anchor: "Readings and Discussion Posts",
-  // },
-
   assignmentsPaperPresentationsTop: {
     href: "/assignments/papers/#paper-presentations",
     anchor: "Paper Presentations",
@@ -61,46 +56,6 @@ export const SiteLinks = {
     href: "/assignments/project/#project",
     anchor: "Project",
   },
-
-  // assignmentsProjectProposalTop: {
-  //   href: "/assignments/project/proposal#project-proposal",
-  //   anchor: "Proposal",
-  // },
-  //
-  // assignmentsProjectMilestoneReport1Top: {
-  //   href: "/assignments/project/milestonereport#project-milestone-report",
-  //   anchor: "Milestone Report 1",
-  // },
-  //
-  // assignmentsProjectMilestoneReport2Top: {
-  //   href: "/assignments/project/milestonereport#project-milestone-report",
-  //   anchor: "Milestone Report 2",
-  // },
-  //
-  // assignmentsProjectFinalReportTop: {
-  //   href: "/assignments/project/finalreport#project-final-report",
-  //   anchor: "Final Report",
-  // },
-  //
-  // assignmentsReflectionsTop: {
-  //   href: "/assignments/reflections/#reflections",
-  //   anchor: "Reflections",
-  // },
-  //
-  // assignmentsReflectionsContributionTop: {
-  //   href: "/assignments/reflections/contribution#contribution-reflection",
-  //   anchor: "Contribution",
-  // },
-  //
-  // assignmentsReflectionsMethodTop: {
-  //   href: "/assignments/reflections/method#method-reflection",
-  //   anchor: "Method",
-  // },
-  //
-  // assignmentsReflectionsFramingTop: {
-  //   href: "/assignments/reflections/framing#framing-reflection",
-  //   anchor: "Framing",
-  // },
 
   calendarTop: {
     href: "/calendar/#calendar",
